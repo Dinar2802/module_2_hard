934fb29d1b3a23cac0cc874fe2ddb584e02e4d3b
Dinar2802 <dinar.raisovich@mail.ru>
2024-08-22 23:03:00 +0300

    Add files via upload

---
+++ module_2_hard_V2.py
@@ -0,0 +1,16 @@
+def pw():
+    password = []
+    n = int(input('Введите число от 3 до 20: '))
+    if n < 3 or n > 20:
+        print('Число не подходит, повторите попытку')
+        pw()
+    else:
+        for i in range(1, n):
+            for j in range(i + 1, n):
+                if n % (i + j) == 0 and i != j:
+                    password.append(str(i) + '+' + str(j))
+
+    print(*password)
+
+
+pw()
